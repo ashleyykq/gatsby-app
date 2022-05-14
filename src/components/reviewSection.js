4a1aba27b10420e3bcd051fd6f2fd9a0eaaa6012
ashley <kqyeong.2018@sis.smu.edu.sg>
2022-05-15 01:30:49 +0800

    edit shadows for review section

--- src/components/reviewSection.js
+++ src/components/reviewSection.js
@@ -1,16 +1,11 @@
-import React, { useRef, useState } from "react"
-// Import Swiper React components
+import React from "react"
 import { Swiper, SwiperSlide } from "swiper/react"
-
-// Import Swiper styles
 import "swiper/css"
 import "swiper/css/effect-coverflow"
 import "swiper/css/pagination"
 import { StaticImage } from "gatsby-plugin-image"
 import SectionLayout from "./sectionLayout"
 import "./reviewSection.css"
-
-// import required modules
 import { EffectCoverflow, Pagination } from "swiper"
 
 export default function reviewSection() {
@@ -26,7 +21,7 @@ export default function reviewSection() {
           stretch: 0,
           depth: 100,
           modifier: 2,
-          slideShadows: true,
+          slideShadows: false,
         }}
         pagination={false}
         loop={true}
@@ -39,7 +34,6 @@ export default function reviewSection() {
               className="quote"
               src="../images/right-quote.png"
               loading="eager"
-              // width={1000}
               height={60}
               quality={100}
               formats={["auto", "webp", "avif"]}
@@ -47,13 +41,11 @@ export default function reviewSection() {
             />
             <div className="content">
               <p>
-                'Quite good massage. recommend owner to change bed please as
-                it's very hard and pressing hard on the face.'
+                Quite good massage. recommend owner to change bed please as it's
+                very hard and pressing hard on the face.
               </p>
               <div className="details">
-                {/* <div> */}
                 <StaticImage
-                  //   className="imgBox"
                   src="../images/avatarIcon.png"
                   loading="eager"
                   height={56}
@@ -61,9 +53,8 @@ export default function reviewSection() {
                   formats={["auto", "webp", "avif"]}
                   alt=""
                 />
-                {/* </div> */}
                 <h3>
-                  name name <br/>
+                  Keith Chu <br />
                   <span>review from Facebook</span>
                 </h3>
               </div>
@@ -76,21 +67,15 @@ export default function reviewSection() {
               className="quote"
               src="../images/right-quote.png"
               loading="eager"
-              // width={1000}
               height={60}
               quality={100}
               formats={["auto", "webp", "avif"]}
               alt=""
             />
             <div className="content">
-              <p>
-                'Quite good massage. recommend owner to change bed please as
-                it's very hard and pressing hard on the face.'
-              </p>
+              <p>Clean and neat environment with awesome skills.</p>
               <div className="details">
-                {/* <div> */}
                 <StaticImage
-                  //   className="imgBox"
                   src="../images/avatarIcon.png"
                   loading="eager"
                   height={56}
@@ -98,9 +83,77 @@ export default function reviewSection() {
                   formats={["auto", "webp", "avif"]}
                   alt=""
                 />
-                {/* </div> */}
                 <h3>
-                  name name <br/>
+                  Janelle Cheeeeyyyyw <br />
+                  <span>review from Facebook</span>
+                </h3>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        {/* <SwiperSlide>
+          <div className="testimonialBox">
+            <StaticImage
+              className="quote"
+              src="../images/right-quote.png"
+              loading="eager"
+              height={60}
+              quality={100}
+              formats={["auto", "webp", "avif"]}
+              alt=""
+            />
+            <div className="content">
+              <p>
+                'I got a shoulder massage here, very professional and quick in
+                setting up appointments. Friendly staff and no over selling. The
+                massage was hard (I had to tell her to soften it a bit but great
+                for people who like strong pressure!) and very balanced. Will
+                definitely go back for footmassage next.'
+              </p>
+              <div className="details">
+                <StaticImage
+                  src="../images/avatarIcon.png"
+                  loading="eager"
+                  height={56}
+                  quality={100}
+                  formats={["auto", "webp", "avif"]}
+                  alt=""
+                />
+                <h3>
+                  Chitrangada Thakore <br />
+                  <span>review from Facebook</span>
+                </h3>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide> */}
+        <SwiperSlide>
+          <div className="testimonialBox">
+            <StaticImage
+              className="quote"
+              src="../images/right-quote.png"
+              loading="eager"
+              height={60}
+              quality={100}
+              formats={["auto", "webp", "avif"]}
+              alt=""
+            />
+            <div className="content">
+              <p>
+                Super clean and comfortable place with friendly staff! Left the
+                massage parlour feeling super fresh and relaxed!
+              </p>
+              <div className="details">
+                <StaticImage
+                  src="../images/avatarIcon.png"
+                  loading="eager"
+                  height={56}
+                  quality={100}
+                  formats={["auto", "webp", "avif"]}
+                  alt=""
+                />
+                <h3>
+                  Seah Yang Wenn <br />
                   <span>review from Facebook</span>
                 </h3>
               </div>
@@ -113,7 +166,6 @@ export default function reviewSection() {
               className="quote"
               src="../images/right-quote.png"
               loading="eager"
-              // width={1000}
               height={60}
               quality={100}
               formats={["auto", "webp", "avif"]}
@@ -121,13 +173,11 @@ export default function reviewSection() {
             />
             <div className="content">
               <p>
-                'Quite good massage. recommend owner to change bed please as
-                it's very hard and pressing hard on the face.'
+                Good service! The masseurs are very skillful and friendly. Will
+                be back again!
               </p>
               <div className="details">
-                {/* <div> */}
                 <StaticImage
-                  //   className="imgBox"
                   src="../images/avatarIcon.png"
                   loading="eager"
                   height={56}
@@ -135,9 +185,8 @@ export default function reviewSection() {
                   formats={["auto", "webp", "avif"]}
                   alt=""
                 />
-                {/* </div> */}
                 <h3>
-                  name name <br/>
+                  Sunny Su <br />
                   <span>review from Facebook</span>
                 </h3>
               </div>
@@ -150,21 +199,15 @@ export default function reviewSection() {
               className="quote"
               src="../images/right-quote.png"
               loading="eager"
-              // width={1000}
               height={60}
               quality={100}
               formats={["auto", "webp", "avif"]}
               alt=""
             />
             <div className="content">
-              <p>
-                'Quite good massage. recommend owner to change bed please as
-                it's very hard and pressing hard on the face.'
-              </p>
+              <p>One word for your massage services - AWESOME!!! 😃</p>
               <div className="details">
-                {/* <div> */}
                 <StaticImage
-                  //   className="imgBox"
                   src="../images/avatarIcon.png"
                   loading="eager"
                   height={56}
@@ -172,46 +215,9 @@ export default function reviewSection() {
                   formats={["auto", "webp", "avif"]}
                   alt=""
                 />
-                {/* </div> */}
                 <h3>
-                  name name <br/>
-                  <span>review from Facebook</span>
-                </h3>
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="testimonialBox">
-            <StaticImage
-              className="quote"
-              src="../images/right-quote.png"
-              loading="eager"
-              // width={1000}
-              height={60}
-              quality={100}
-              formats={["auto", "webp", "avif"]}
-              alt=""
-            />
-            <div className="content">
-              <p>
-                'Quite good massage. recommend owner to change bed please as
-                it's very hard and pressing hard on the face.'
-              </p>
-              <div className="details">
-                {/* <div> */}
-                <StaticImage
-                  //   className="imgBox"
-                  src="../images/avatarIcon.png"
-                  loading="eager"
-                  height={56}
-                  quality={100}
-                  formats={["auto", "webp", "avif"]}
-                  alt=""
-                />
-                {/* </div> */}
-                <h3>
-                  name name <br/>
+                  Norimah Abdullah
+                  <br />
                   <span>review from Facebook</span>
                 </h3>
               </div>
